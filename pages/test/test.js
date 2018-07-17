cf43c9ba31d7465f67f88cac068db8ac98921c6d
lvjingzhi <625679084@qq.com>
2018-07-17 16:59:41 +0800

    加了一张轮播图，测试了下列表渲染

--- pages/test/test.js
+++ pages/test/test.js
@@ -1,5 +1,14 @@
 Page({
   data:{
-   msg:'这是一个测试！'
+   msg:['这是一个测试','这是两个测试','这是三个测试'],
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
   }
 })
